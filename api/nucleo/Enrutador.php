9cb5b87dd313a190a9328b39fe4395481f087cbe
Jhon Alexander Cardona Lopez <jcl0040@alu.medac.es>
2026-04-14 00:02:49 +0200

    commit

--- api/nucleo/Enrutador.php
+++ api/nucleo/Enrutador.php
@@ -12,9 +12,7 @@ class Enrutador {
     }
 
     public function agregarRuta($metodo_http, $ruta, $clase, $metodo_clase) {
-        $this->rutas[] = [
-            'metodo_http' => $metodo_http,
-            'ruta' => $this->entidad . $ruta,
+        $this->rutas[$metodo_http][$this->entidad . $ruta] = [
             'clase' => $clase,
             'metodo_clase' => $metodo_clase
         ];
@@ -22,41 +20,37 @@ class Enrutador {
 
     public function procesarRuta() {
 
-        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $metodo_http = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-        foreach ($this->rutas as $ruta) {
+        if (isset($this->rutas[$metodo_http][$uri])) {
+            
+            $ruta = $this->rutas[$metodo_http][$ruta];
+            $clase = $ruta['clase'];
+            $metodo_clase = $ruta['metodo_clase'];
 
-            if ($ruta['metodo_http'] === $metodo_http && $ruta['ruta'] === $uri) {
-                $clase = $ruta['clase'];
-                $metodo_clase = $ruta['metodo_clase'];
+            if (class_exists($clase)) {
+                $controlador = new $clase();
+                return $controlador->$metodo_clase();
+            } else {
+                http_response_code(500);
 
-                if (class_exists($clase)) {
-                    $controlador = new $clase();
-
-                    return $controlador->$metodo_clase();
-                } else {
-
-                    http_response_code(500);
-
-                    $respuesta = [
-                        'success' => false,
-                        'message' => 'Controlador no encontrado'
-                    ];
-                    
-                     echo json_encode($respuesta);
-                     return;
-                }
+                $respuesta = [
+                    'success' => false,
+                    'message' => 'Controlador no encontrada'
+                ];
+                
+                echo json_encode($respuesta);
             }
+        } else {
+            http_response_code(404);
+
+            $respuesta = [
+                'success' => false,
+                'message' => 'Ruta no encontrada'
+            ];
+
+            echo json_encode($respuesta);
         }
-
-        http_response_code(404);
-
-        $respuesta = [
-            'success' => false,
-            'message' => 'Ruta no encontrada'
-        ];
-
-        echo json_encode($respuesta);
     }
 }
