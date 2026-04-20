2ec9062acb0de1617ea5383cc1f75ceb095b0a9b
Jhon Alexander Cardona Lopez <jcl0040@alu.medac.es>
2026-04-20 03:20:24 +0200

    Commit

---
+++ negocio/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cometta/cometta',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ed2e06b389a537e82ed8a046e0ad200ccb6a8644',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cometta/cometta' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ed2e06b389a537e82ed8a046e0ad200ccb6a8644',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
